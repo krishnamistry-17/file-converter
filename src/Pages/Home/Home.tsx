960793ca1fa7e03d0d9a70c1e1b70261d8ce9c0a
krishnamistry-17 <mistrykrishna17103@gmail.com>
2026-01-27 18:32:17 +0530

    integarted custome text-editor package and used it

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,10 +2,15 @@ import Herosection from "./Herosection";
 import ConvertTabs from "../../components/ConvertTabs";
 import Detail from "./Detail/Detail";
 import ConversionFlow from "./Detail/ConversionFlow";
+// import { Editor } from "@test-my/text-editor";
+// import "@test-my/text-editor/dist/style.css";
 
 const Home = () => {
   return (
     <div className="w-full bg-linear-to-b from-gray-50 to-white">
+      {/* <div className="my-4">
+        <Editor defaultTheme="light" />
+      </div> */}
       <section>
         <Herosection />
       </section>
